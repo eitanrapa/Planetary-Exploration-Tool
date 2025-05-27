bfd8ca84fd3562be2ca439f795a1305874f90679
bene_an <andreas.benedikter@dlr.de>
2025-05-27 20:16:36 +0200

    added foundational campaign

--- pkg/pet/campaigns/orbiter/__init__.py
+++ pkg/pet/campaigns/orbiter/__init__.py
@@ -6,5 +6,5 @@
 # (c) 2023-2025 all rights reserved
 
 from .Nightingale5to1 import Nightingale5to1 as nightingale5to1
-
+from .NightingaleFoundational import NightingaleFoundational as nightingaleFoundational
 # end of file
